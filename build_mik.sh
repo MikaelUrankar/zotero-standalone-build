6f848cb8f262075d67a25c2d4e49838a2d63bd08
pouet <mikael.urankar@gipsa-lab.grenoble-inp.fr>
2022-03-12 18:19:15 +0100

    Add custom build script

---
+++ build_mik.sh
@@ -0,0 +1,2 @@
+/root/zotero/zotero-build/xpi/build_xpi -s /root/zotero/zotero-client/build -c release
+/root/zotero/zotero-standalone-build/build.sh -d /root/zotero/zotero-build/xpi/build/staging -p f -c release
